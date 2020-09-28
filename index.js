782da0b7eef1e3f2b00fc6eadfa1d1e5561f4fbd
Viachaslau Rouski <46630463+vrovsky@users.noreply.github.com>
2020-09-28 22:05:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,44 @@
 // while(numbers.length > 0) numbers.pop();
 // console.log(numbers);
 
-const first = [1, 2, 3];
-const second = [1, 2, 3];
-const combined = first.concat(second);
+// const first = [1, 2, 3];
+// const second = [1, 2, 3];
+// const combined = first.concat(second);
+// // for(let number in combined){console.log(number);}
+// combined.forEach(function(number){console.log(number);});
+// combined.forEach(number => console.log(number));
 
-// for(let number in combined){console.log(number);}
-combined.forEach(function(number){console.log(number);});
-combined.forEach(number => console.log(number));
+// const numbers = [1, 2, 3];
+// const joined = numbers.join(',');
+// console.log(joined);
+// const message = 'This is my first message';
+// const parts = message.split(' ');
+// console.log(parts);
+// const combined = parts.join('-');
+// console.log(combined);
+
+// const arrayOfObjects = [
+//     {id: 1, name: 'Node.js'},
+//     {id: 2, name: 'javaScript'}
+// ]
+// arrayOfObjects.sort(function(a,b){
+//     const nameA = a.name.toLowerCase();
+//     const nameB = b.name.toLowerCase();
+
+//     if (nameA < name) return -1;
+//     if (nameA > name) return 1;
+//     else return 0;
+
+// });
+// console.log(arrayOfObjects); 
+
+// const numbers = [1, 2, 3];
+// let allPositive = numbers.every(function(value){
+//     return value >= 0;
+// });
+// console.log(allPositive);
+
+const numbers = [1, 2, -1, 3];
+const filtered = numbers.filter(value => value >= 0
+)
+console.log(filtered);
