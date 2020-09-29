6bf30b0120dd529638ebc3a75a2244841dae9620
Viachaslau Rouski <46630463+vrovsky@users.noreply.github.com>
2020-09-29 23:21:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,15 @@
 // console.log(allPositive);
 
 const numbers = [1, 2, -1, 3];
-const filtered = numbers.filter(value => value >= 0
-)
-console.log(filtered);
+const filtered = numbers.filter(n => n >= 0);
+const items = filtered.map(n => '<li>' + n + '</li>');
+
+
+const html = '<ul>' + items.join('') + '</ul>';  
+console.log(html);
+
+// Filtered() and map() are chainable
+
+numbers
+.filter(n => n >= 0)
+.map(n => '<li>' + n + '</li>');
